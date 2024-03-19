3e37394c502693421f1431c39d4e7e7e2aaf3caf
simone-nolberto <simone.nolberto@outlook.com>
2024-03-19 14:47:18 +0100

    update with working base exercise

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
 console.log('it works!');
+
+const { createApp } = Vue
+createApp({
+    data() {
+        return {
+
+            randomEmailList: [],
+        }
+    },
+    methods: {
+        generateRandomEmail() {
+            for (let i = 0; i < 10; i++) {
+                axios
+                    .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                    .then((response) => {
+                        console.log(response.data.response);
+                        this.randomEmailList.push(response.data.response);
+    
+                    });
+            };
+            
+            console.log(this.randomEmailList);
+        }
+
+    }
+}).mount('#app')
